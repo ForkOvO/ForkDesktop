9641a4067f9c1e025ffbc84048af9c27e2c1b102
kangkang <2985385632@qq.com>
2024-12-20 14:29:43 +0800

    侧边栏实现样式，添加当前高亮

--- window/sidebar.cpp
+++ window/sidebar.cpp
@@ -1,8 +1,10 @@
 #include "sidebar.h"
+#include "qssmanager.h"
 
 #include <QPropertyAnimation>
 #include <QPushButton>
 #include <QLabel>
+#include <QFile>
 
 Sidebar::Sidebar(QWidget *parent)
     : QWidget(parent)
@@ -18,19 +20,54 @@ Sidebar::Sidebar(QWidget *parent)
     m_foldAnimation = new QPropertyAnimation(this, "geometry");
     m_foldAnimation->setDuration(300);
 
+    // 背景
+    m_background = new QWidget(this);
+    m_background->setObjectName("sidebarBackground");
+    m_background->setGeometry(0, 0, 50, 50);
+    // 点击动画
+    m_clickAnimation = new QPropertyAnimation(m_background, "geometry");
+    m_clickAnimation->setDuration(500);
+
     // 侧边栏按钮
     for (int i = 0; i < btnList.size(); i++)
     {
         // 按钮
-        QPushButton *btn = new QPushButton(btnList[i], this);
-        btn->setObjectName(btnList[i]);
+        QPushButton *btn = new QPushButton(this);
+        btn->setObjectName(btnList[i] + "SidebarBtn");
         btn->setGeometry(0, 50 * i, 50, 50);
-        connect(btn, &QPushButton::clicked, this, [i, this](){btnClicked(i);});
+        connect(btn, &QPushButton::clicked, this, [i, this]{this->onBtnClicked(i);});
         // 标签
         QLabel *label = new QLabel(btnNameList[i], this);
-        label->setAlignment(Qt::AlignCenter);
+        label->setObjectName(btnList[i] + "SidebarLabel");
+        label->setAlignment(Qt::AlignVCenter);
         label->setGeometry(50, 50 * i, 100, 50);
     }
+
+    // QSS模板
+    m_qssManager = QSSManager::instance();
+    QFile file(":/qss/sidebar.qss");
+    file.open(QFile::ReadOnly);
+    QString qssTemplate(file.readAll());
+    file.close();
+    // 应用QSS
+    QString allQss;
+    allQss += "QWidget#sidebarBackground{ background-color:#80808080; border-radius: 10px; }";
+    for (QString name: btnList)
+    {
+        allQss += QString(qssTemplate).replace("{{name}}", name);
+    }
+    m_qssManager->input(this, QString(allQss).replace("{{theme}}", "white"), QString(allQss).replace("{{theme}}", "black"));
+}
+
+void Sidebar::onBtnClicked(int index)
+{
+    emit btnClicked(index);
+    // 点击动画移动背景
+    QRect rect = m_background->geometry();
+    m_clickAnimation->setStartValue(rect);
+    rect.moveTo(0, 50 * index);
+    m_clickAnimation->setEndValue(rect);
+    m_clickAnimation->start();
 }
 
 void Sidebar::enterEvent(QEnterEvent *event)
--- window/sidebar.h
+++ window/sidebar.h
@@ -5,7 +5,10 @@
 
 #include <QWidget>
 
+// Qt
 class QPropertyAnimation;
+// 自定义
+class QSSManager;
 
 class Sidebar : public QWidget
 {
@@ -16,12 +19,18 @@ public:
 signals:
     void btnClicked(int index);
 
+private slots:
+    void onBtnClicked(int index);
+
 protected:
     void enterEvent(QEnterEvent *event) override;
     void leaveEvent(QEvent *event) override;
 
 private:
     QPropertyAnimation* m_foldAnimation; // 折叠动画
+    QPropertyAnimation* m_clickAnimation; // 点击动画
+    QSSManager* m_qssManager; // QSS管理器
+    QWidget* m_background; // 背景
 };
 
 #endif // SIDEBAR_H
